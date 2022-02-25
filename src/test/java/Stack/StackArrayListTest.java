5e01fcd06595b069c2da702e187b86044929885a
CindyGualim <gua21226@uvg.edu.gt>
2022-02-24 19:04:33 -0600

    Create StackArrayListTest.java
    Junit List

---
+++ src/test/java/Stack/StackArrayListTest.java
@@ -0,0 +1,58 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Stack;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author cindy
+ */
+public class StackArrayListTest {
+    
+    public StackArrayListTest() {
+    }
+    
+     Stack<Double> stack;
+    @BeforeEach
+    void setUp() {
+        stack = new StackVector<Double>();
+    }
+
+    @Test
+    void remove() {
+        stack.add(5.27);
+        stack.add(7.97);
+
+        assertEquals(7.97, stack.remove());
+    }
+
+    @Test
+    void peek() {
+        stack.add(5.27);
+        stack.add(7.97);
+
+        assertEquals(7.97, stack.peek());
+    }
+
+    @Test
+    void empty() {
+        assertEquals(true, stack.empty());
+    }
+
+    @Test
+    void size() {
+        stack.add(5.27);
+        stack.add(7.97);
+
+        assertEquals(2, stack.size());
+    }
+}
